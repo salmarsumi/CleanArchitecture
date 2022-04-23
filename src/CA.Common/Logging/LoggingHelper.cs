1e04c0fb41ddc060ad8f132bef1c57703287fd59
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2022-04-23 20:21:33 +0300

    add a serilog configuration helper

---
+++ src/CA.Common/Logging/LoggingHelper.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace CA.Common.Logging
+{
+    public static class LoggingHelper
+    {
+        public static LoggerConfiguration CASerilogConfiguration(string appName)
+        {
+            var config = new LoggerConfiguration();
+
+            config
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning);
+
+            config.Enrich.WithProperty("ApplicationContext", appName)
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .WriteTo.Console(outputTemplate:
+                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:w4}]: {Message:lj} {CorrelationId}{NewLine}{Exception}");
+
+            return config;
+        }
+    }
+}
