8cb1b46dea4f29f5bdc1951e388d6ef8922d3cda
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2022-06-18 02:00:19 +0300

    hostname in certificate instead of ip

--- docker/create-cert.sh
+++ docker/create-cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export EXTERNAL_HOST_IP=$1 
+export EXTERNAL_HOST=$1
 mkdir -p ./certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/server.key -out ./certs/server.crt -config ca.cnf -subj /CN=$1
 
