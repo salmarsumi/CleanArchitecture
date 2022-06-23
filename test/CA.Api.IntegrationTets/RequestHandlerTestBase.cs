c49c71b0cc8e4dfe884551cf5f6407e74a6400bd
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2022-06-23 14:04:02 +0300

    query integration tests

--- test/CA.Api.IntegrationTets/RequestHandlerTestBase.cs
+++ test/CA.Api.IntegrationTets/RequestHandlerTestBase.cs
@@ -1,5 +1,9 @@
-﻿using CA.Api.Infrastructure.Data;
+﻿using CA.Api.Application.WeatherForcast.Queries;
+using CA.Api.Domain.Interfaces;
+using CA.Api.Infrastructure.Data;
+using CA.Api.Infrastructure.Queries;
 using CA.Api.Infrastructure.Repositories;
+using System.Reflection;
 
 namespace CA.Api.IntegrationTets
 {
@@ -10,9 +14,25 @@ namespace CA.Api.IntegrationTets
 
         public RequestHandlerTestBase()
         {
-            IApiDbContext context = CreateApiContext();
-            var respository = new WeatherForecastRepository(CreateApiContext());
-            _handler = Activator.CreateInstance(typeof(T), respository) as T;
+            var parameters = new List<object>();
+            ConstructorInfo constructor = typeof(T).GetConstructors().First();
+            
+            foreach(ParameterInfo p in constructor.GetParameters())
+            {
+                if(p.ParameterType == typeof(IWeatherForecastRepository))
+                {
+                    var repository = new WeatherForecastRepository(CreateApiContext());
+                    parameters.Add(repository);
+                }
+                else if(p.ParameterType == typeof(IWeatherForecastQueries))
+                {
+                    var query = new WeatherForecastQueries(CreateApiContext());
+                    parameters.Add(query);
+                }
+
+            }
+
+            _handler = Activator.CreateInstance(typeof(T), parameters.ToArray()) as T;
         }
 
         protected ApiDbContext CreateApiContext()
--- test/CA.Api.IntegrationTets/WeatherForecast/Queries/GetAllWeatherForcastTests.cs
+++ test/CA.Api.IntegrationTets/WeatherForecast/Queries/GetAllWeatherForcastTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CA.Api.Application.WeatherForcast.Queries;
+using CA.Api.Infrastructure.Data;
+using CA.MediatR;
+using Entities = CA.Api.Domain.Entities;
 
 namespace CA.Api.IntegrationTets.WeatherForecast.Queries
 {
-    internal class GetAllWeatherForcastTests
+    public class GetAllWeatherForcastTests : RequestHandlerTestBase<GetAllWeatherForcastQueryHandler>
     {
+        [Fact]
+        public async Task Handle_ReturnsWeatherForecasts_WhenCommandIsValid()
+        {
+            // Arrange
+            IApiDbContext context = CreateApiContext();
+            var entity = new Entities.WeatherForecast
+            {
+                Created = DateTime.Now,
+                CreatedBy = "Test User",
+                Date = DateTime.Now,
+                LastModified = DateTime.Now,
+                LastModifiedBy = "Test User",
+                Summary = "Summary",
+                TemperatureC = 50
+            };
+            context.WeatherForcasts.Add(entity);
+            await context.SaveChangesAsync(CancellationToken.None);
+
+            var query = new GetAllWeatherForcastQuery();
+
+            // Act
+            RequestResult<IEnumerable<WeatherForecastDto>> result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.Success);
+            Assert.Contains(result.Result, x => x.Id == entity.Id);
+        }
     }
 }
