115ff93e491f2172170a63beb1c90d9db19f2a14
Saad Al-Marsumi <smd_almarsumi@hotmail.com>
2022-07-03 19:41:52 +0300

    separate certificate creation fron image building.

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-export EXTERNAL_HOST_NAME=$1
-mkdir -p ./certs
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/server.key -out ./certs/server.crt -config ca.cnf -subj /CN=$1
-
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --no-cache
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc) --force
